e55c8d96813169ae08567540004e229c403df25a
tannerhbrewer <tannerhbrewer@gmail.com>
2023-11-22 13:13:41 -0500

    Implemented ImGui events.

--- Matter/src/Matter/Core/Base.h
+++ Matter/src/Matter/Core/Base.h
@@ -9,3 +9,5 @@
 #endif
 
 #define BIT(x) (1 << x)
+
+#define MATTER_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
--- Matter/src/Matter/ImGui/ImGuiLayer.h
+++ Matter/src/Matter/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Matter/Core/Layer.h"
 
+#include "Matter/Events/ApplicationEvent.h"
+#include "Matter/Events/KeyEvent.h"
+#include "Matter/Events/MouseEvent.h"
+
 namespace Matter {
 
 	class ImGuiLayer : public Layer {
@@ -14,6 +18,16 @@ namespace Matter {
 		void Detach();
 		void Update();
 		void OnEvent(Event& event);
+
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 
 	private:
 		float m_Time = 0.0f;
