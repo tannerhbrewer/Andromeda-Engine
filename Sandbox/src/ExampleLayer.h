77c9a7cf28a86c9534bd916be33dada2b5dcc601
tannerhbrewer <tannerhbrewer@gmail.com>
2023-11-23 10:07:56 -0500

    Implemented shader transforms.

--- Sandbox/src/ExampleLayer.h
+++ Sandbox/src/ExampleLayer.h
@@ -4,6 +4,8 @@
 
 #include "imgui/imgui.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 class ExampleLayer : public Matter::Layer {
 
 public:
@@ -34,10 +36,10 @@ public:
 		m_SquareVA.reset(Matter::VertexArray::Create());
 
 		float squareVertices[3 * 4] = {
-			-0.75f, -0.75f, 0.0f,
-			 0.75f, -0.75f, 0.0f,
-			 0.75f,  0.75f, 0.0f,
-			-0.75f,  0.75f, 0.0f
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f
 		};
 
 		std::shared_ptr<Matter::VertexBuffer> squareVB;
@@ -57,14 +59,18 @@ public:
 			
 			layout(location = 0) in vec3 a_Position;
 			layout(location = 1) in vec4 a_Color;
+
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
+
 			out vec3 v_Position;
 			out vec4 v_Color;
+
 			void main()
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
 			}
 		)";
 
@@ -87,12 +93,16 @@ public:
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
+
 			out vec3 v_Position;
+
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
 			}
 		)";
 
@@ -136,7 +146,20 @@ public:
 
 		Matter::Renderer::BeginScene(m_Camera);
 
-		Matter::Renderer::Submit(m_BlueShader, m_SquareVA);
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for (int y = 0; y < 20; y++) {
+
+			for (int x = 0; x < 20; x++) {
+
+				glm::vec3 position(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * scale;
+				Matter::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+
+			}
+
+		}
+
 		Matter::Renderer::Submit(m_Shader, m_VertexArray);
 
 		Matter::Renderer::EndScene();
