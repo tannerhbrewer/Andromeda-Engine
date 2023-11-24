3c3561d8a3c5c57e871f6e746c9c685a0a762b73
tannerhbrewer <tannerhbrewer@gmail.com>
2023-11-23 22:24:20 -0500

    Combined textured and none textured quads into one shader.

--- Matter/src/Matter/Renderer/Texture.cpp
+++ Matter/src/Matter/Renderer/Texture.cpp
@@ -7,12 +7,26 @@
 
 namespace Matter {
 
+	Ref<Texture2D> Texture2D::Create(uint32_t width, uint32_t height) {
+
+		switch (Renderer::GetAPI()) {
+
+			case RendererAPI::API::None:	MATTER_ASSERT(false, "RendererAPI::None is currently not supported."); return nullptr;
+			case RendererAPI::API::OpenGL:	return CreateRef<OpenGLTexture2D>(width, height);
+
+		}
+
+		MATTER_ASSERT(false, "Unknown renderer API.");
+		return nullptr;
+
+	}
+
 	Ref<Texture2D> Texture2D::Create(const std::string& path) {
 
 		switch (Renderer::GetAPI()) {
 
 			case RendererAPI::API::None:	MATTER_ASSERT(false, "RendererAPI::None is currently not supported."); return nullptr;
-			case RendererAPI::API::OpenGL:	return std::make_shared<OpenGLTexture2D>(path);
+			case RendererAPI::API::OpenGL:	return CreateRef<OpenGLTexture2D>(path);
 
 		}
 
